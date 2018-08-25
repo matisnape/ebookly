df1d036e0eb400cc876a7c1dc36d7f58fe9148a7
Anna Anks Nowak <matisnape@wp.pl>
2018-08-25 18:44:21 +0200

    add support for creating authors from index

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,40 @@
 class AuthorsController < ApplicationController
   def index
     @authors = Author.all
+    @author = Author.new
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @authors = Author.all
+    @author = Author.new(author_params)
+    respond_to do |format|
+      if @author.save
+        format.html { redirect_to @author, notice: 'Author created.' }
+        format.js
+        format.json { render json: @author, status: :created, location: @author }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @author = Author.destroy(params[:id])
+    redirect_to authors_path
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name)
+  end
+
+  def author
+    @author = Author.find(params[:id])
   end
 end
