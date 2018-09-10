a4ea86bb1054659b5a5727fb106b6116da3dd20a
Anna Anks Nowak <matisnape@wp.pl>
2018-09-10 22:26:26 +0200

    [Shop] add custom validation for slug

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,7 +2,8 @@ class Shop < ApplicationRecord
   has_many :books, dependent: :destroy
 
   validates :name, :slug, presence: true
-  validates_uniqueness_of :name, :slug
+  validates_uniqueness_of :name, { message: "Name is already taken" }
+  validate :uniqueness_of_slug
 
   before_validation :to_slug
 
@@ -14,4 +15,10 @@ class Shop < ApplicationRecord
     self.slug = name&.parameterize
   end
   alias_method :to_param, :to_slug
+
+  def uniqueness_of_slug
+    if name and Shop.exists?(slug: to_slug)
+      errors.add(:name, 'is not unique enough. Maybe this shop already exists?')
+    end
+  end
 end
