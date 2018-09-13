24c2cb3c104b9a7e06bd34a74410425b6bf752de
Anna Anks Nowak <matisnape@wp.pl>
2018-09-13 21:50:33 +0200

    [Shop] extract check for existing slug to a method

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -19,8 +19,14 @@ class Shop < ApplicationRecord
   alias_method :to_param, :to_slug
 
   def uniqueness_of_slug
-    if name and Shop.where.not(id: id).exists?(slug: to_slug)
+    if slug_exists?
       errors.add(:name, 'is not unique enough. Maybe this shop already exists?')
     end
+  end
+
+  private
+
+  def slug_exists?
+    name && Shop.where.not(id: id).exists?(slug: to_slug)
   end
 end
