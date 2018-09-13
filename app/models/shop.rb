86224ea08c7f55a437c4603784c9444763705255
Anna Anks Nowak <matisnape@wp.pl>
2018-09-13 20:28:35 +0200

    [Shop] fix test for slug uniqueness - exclude current shop from query

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -17,7 +17,7 @@ class Shop < ApplicationRecord
   alias_method :to_param, :to_slug
 
   def uniqueness_of_slug
-    if name and Shop.exists?(slug: to_slug)
+    if name and Shop.where.not(id: id).exists?(slug: to_slug)
       errors.add(:name, 'is not unique enough. Maybe this shop already exists?')
     end
   end
