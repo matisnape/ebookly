9c6c9942835ad709677857800166f8193c3333e2
Anna Anks Nowak <matisnape@wp.pl>
2018-12-08 23:45:27 +0100

    Add basic errors

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::BaseController < ApplicationController
+  def invalid_resource!(errors = [])
+    api_error(status: 422, errors: errors)
+  end
+
+  def not_found!
+    return api_error(status: 404, errors: 'Not found')
+  end
 end
